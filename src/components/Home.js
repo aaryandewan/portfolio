0e3befb267f088bb94afd73230d739c5141ba4cb
Aaryan Dewan <aaryandewan@yahoo.com>
2021-12-31 14:49:27 +0530

    homepage edit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="HomeDiv">
+      <div className="Home">
+        <h2 className="Hi">Hi, I am </h2>
+        <h1 className="gradient-text">AARYAN</h1>
+        <h2 className="WebStuff">And I build stuff for the web</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
